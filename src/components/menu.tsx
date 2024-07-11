a43a069c08089bfa7cb75141b58a1f3435210859
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-11 12:18:55 +0100

    Update menu.tsx

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -6,49 +6,45 @@ import Settings from './icons/Settings';
 import Mine from './icons/Mine';
 import Friends from './icons/Friends';
 import Coins from './icons/Coins';
+import { binanceLogo, hamsterCoin } from './images'; // Ensure these are correctly imported
 
 export const Menu = () => {
   return (
     <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
-      <NavLink
-        to="/"
-        className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl"
-        activeClassName="text-white" // Optional: add styles for active link
-      >
-        <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
-        <p className="mt-1">Home</p>
+
+      <NavLink to="/" className="w-1/5">
+        <div className="text-center text-[#85827d] bg-[#1c1f24] m-1 p-2 rounded-2xl">
+          <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Home</p>
+        </div>
       </NavLink>
-      <NavLink
-        to="/mine"
-        className="text-center text-[#85827d] w-1/5"
-        activeClassName="text-white" // Optional: add styles for active link
-      >
-        <Mine className="w-8 h-8 mx-auto" />
-        <p className="mt-1">Mine</p>
+
+      <NavLink to="#" className="w-1/5">
+        <div className="text-center text-[#85827d]">
+          <Mine className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Mine</p>
+        </div>
       </NavLink>
-      <NavLink
-        to="/friends"
-        className="text-center text-[#85827d] w-1/5"
-        activeClassName="text-white" // Optional: add styles for active link
-      >
-        <Friends className="w-8 h-8 mx-auto" />
-        <p className="mt-1">Friends</p>
+
+      <NavLink to="/game" className="w-1/5">
+        <div className="text-center text-[#85827d]">
+          <Friends className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Play</p>
+        </div>
       </NavLink>
-      <NavLink
-        to="/earn"
-        className="text-center text-[#85827d] w-1/5"
-        activeClassName="text-white" // Optional: add styles for active link
-      >
-        <Coins className="w-8 h-8 mx-auto" />
-        <p className="mt-1">Earn</p>
+
+      <NavLink to="#" className="w-1/5">
+        <div className="text-center text-[#85827d]">
+          <Coins className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Earn</p>
+        </div>
       </NavLink>
-      <NavLink
-        to="/airdrop"
-        className="text-center text-[#85827d] w-1/5"
-        activeClassName="text-white" // Optional: add styles for active link
-      >
-        <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
-        <p className="mt-1">Airdrop</p>
+
+      <NavLink to="#" className="w-1/5">
+        <div className="text-center text-[#85827d]">
+          <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Airdrop</p>
+        </div>
       </NavLink>
     </div>
   );
