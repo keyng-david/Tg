242494a00fffd196edf4bda212ccf5d2c190e151
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-13 07:08:13 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,3 @@
-import { useQuery, useMutation } from 'convex/react';
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Toaster } from "sonner";
